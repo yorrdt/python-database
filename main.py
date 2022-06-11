31c89ed3e170d11062fd69ec55dd2725cc85c3c0
yorrdt <this.mazahaka@gmail.com>
2022-06-11 14:08:56 +0300

    filter menu

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from getkey import getkey, keys
 
 from database import Database
 
-commands_list = ["1. Добавить запись", "2. Редактировать запись",
-                 "3. Удалить запись", "4. Вывести записи",
+commands_list = ["1. Добавить запись", "2. Редактировать запись", "3. Удалить запись", "4. Вывести записи",
                  "5. Фильтрация записей", "6. Справка", "7. Выход"]
 
+filter_list = ["1. Номер записи", "2. Нефтепродукт", "3. Объем", "4. Объем поставок в год",
+               "5. Годовое потребление", "6. Количество через 5 лет"]
 
-def clearConsole():
+
+def clear_console():
     if os.name == ("nt", "dos"):
         os.system("cls")
         return
     os.system("clear")
 
 
-clearConsole()
+clear_console()
 
 databaseObj = Database()
 selectedItem = 0
 
-def drawMenu(item):
+
+def draw_menu(item):
     print("Меню: ")
     for i in range(0, len(commands_list)):
         if i == item:
@@ -32,20 +35,52 @@ def drawMenu(item):
     print("Для выбора подходящего пункта меню нажмите клавишу ENTER")
 
 
+def draw_filter_menu(item):
+    print("|" + 30 * "-" + "| Фильтрация записей |" + 30 * "-" + "|")
+    print("| " + "Выберите параметр фильтрации: \n")
+    for i in range(0, len(filter_list)):
+        if i == item:
+            print(f"> {filter_list[i]}")
+        else:
+            print(f"{filter_list[i]}")
+    print("\n|\n| Для перемещения по меню используйте клавиши UP и DOWN")
+    print("| Для выбора подходящего пункта меню нажмите клавишу ENTER")
+    print("|" + 82 * "-" + "|")
+
+
 def on_press_down():
     global selectedItem
-    clearConsole()
+    clear_console()
     if selectedItem < len(commands_list) - 1:
         selectedItem += 1
-    drawMenu(selectedItem)
+    draw_menu(selectedItem)
 
 
 def on_press_up():
     global selectedItem
-    clearConsole()
+    clear_console()
     if selectedItem > 0:
         selectedItem -= 1
-    drawMenu(selectedItem)
+    draw_menu(selectedItem)
+
+
+filterSelectedItem = 0
+
+
+def on_press_down_filter():
+    global filterSelectedItem
+    clear_console()
+    if filterSelectedItem < len(filter_list) - 1:
+        filterSelectedItem += 1
+    draw_filter_menu(filterSelectedItem)
+
+
+def on_press_up_filter():
+    global filterSelectedItem
+    clear_console()
+    if filterSelectedItem > 0:
+        filterSelectedItem -= 1
+    draw_filter_menu(filterSelectedItem)
 
 
 def wait_press(pressed_key):
@@ -54,7 +89,7 @@ def wait_press(pressed_key):
         key = getkey()
 
 
-def showIntroScreen():
+def show_intro_screen():
     print("|" + 27 * "-" + "| Информационная заставка |" + 28 * "-" + "|")
     print("|" + " ".center(82) + "|")
     print("| " + "\"База данных автоматизации учёта и прогноза запасов нефтепродуктов,".center(80) + " |")
@@ -62,17 +97,25 @@ def showIntroScreen():
     print("|" + " ".center(82) + "|")
     print("|" + "Разработчик: Мороз Егор Владимирович".center(82) + "|")
 
+    # something
 
     print("|" + 21 * "-" + "[ Нажмите ENTER для начала работы с БД ]".center(40) + 21 * "-" + "|")
     wait_press(keys.ENTER)
-    clearConsole()
-    drawMenu(0)
+    clear_console()
+    draw_menu(0)
 
 
-showIntroScreen()
+show_intro_screen()
 
 
-def menuConditions(menu_item):
+def filter_conditions(selectedItem):
+    clear_console()
+    print(selectedItem)
+    global filterSelectedItem
+    filterSelectedItem = 0
+
+
+def menu_conditions(menu_item):
 
     if menu_item == 0:
         print("|" + 28 * "-" + "| Добавление новой записи |" + 27 * "-" + "|")
@@ -107,22 +150,22 @@ def menuConditions(menu_item):
 
     elif menu_item == 3:
 
-        print("|" + 82 * "-" + "|")
+        print("|" + 80 * "-" + "|")
 
         print("| " + "ID".center(4) + " | "
               + "Нефтепродукт".center(12) + " | "
               + "Объём".center(10) + " | "
               + "Объём поставок".center(14) + " | "
               + "Годовое".center(11) + " | "
-              + "Кол-во через".center(14) + " |")
+              + "Кол-во через".center(12) + " |")
         print("| " + 4 * " " + " | "
               + 12 * " " + " | "
               + 10 * " " + " | "
               + "в год".center(14) + " | "
               + "потребление".center(11) + " | "
-              + "5 лет".center(14) + " |")
+              + "5 лет".center(12) + " |")
 
-        print("|" + 82 * "-" + "|")
+        print("|" + 80 * "-" + "|")
 
         entries = databaseObj.getEntries
         for i in range(0, len(entries)):
@@ -131,12 +174,24 @@ def menuConditions(menu_item):
             print(f"{entries[i][2]:<10}", end=" | ")
             print(f"{entries[i][3]:<14}", end=" | ")
             print(f"{entries[i][4]:<11}", end=" | ")
-            print(f"{entries[i][5]:<14}", end=" |\n")
+            print(f"{entries[i][5]:<12}", end=" |\n")
 
-        print("|" + 82 * "-" + "|")
+        print("|" + 80 * "-" + "|")
 
     elif menu_item == 4:
-        print("filter")
+
+        draw_filter_menu(0)
+
+        filter_key = keys.L
+        while filter_key != keys.ENTER:
+            filter_key = getkey()
+            if filter_key == keys.DOWN:
+                on_press_down_filter()
+            elif filter_key == keys.UP:
+                on_press_up_filter()
+
+        filter_conditions(filterSelectedItem)
+
     elif menu_item == 5:
         print("help")
     elif menu_item == 6:
@@ -155,13 +210,13 @@ while True:
         elif key == keys.UP:
             on_press_up()
 
-    clearConsole()
-    menuConditions(selectedItem)
+    clear_console()
+    menu_conditions(selectedItem)
     selectedItem = 0
     print("\nНажмите клавишу ESC для перехода на главное меню")
     wait_press(keys.ESC)
-    clearConsole()
-    drawMenu(0)
+    clear_console()
+    draw_menu(0)
 
 
 
